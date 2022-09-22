f21f6fad3087ec5ff09f138607c9a78cd45ac3ed
Angel Murillo <angel.murillo@mykenzie.snhu.edu>
2022-09-22 16:39:11 -0700

    Completed

--- src/main/java/com/kenzie/app/Main.java
+++ src/main/java/com/kenzie/app/Main.java
@@ -3,6 +3,15 @@ package com.kenzie.app;
 import java.util.Scanner;
 
 public class Main {
+
+    public static void displayMenu() {
+        System.out.println("Enter a selection (1-4)");
+        System.out.println("1. Check your Balance");
+        System.out.println("2. Buy");
+        System.out.println("3. Sell");
+        System.out.println("4. Exit");
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to the Kenzie Stock Exchange");
         Scanner in = new Scanner(System.in);
@@ -14,13 +23,48 @@ public class Main {
         double price = Double.parseDouble(in.nextLine());
 
         // Create your StockHolding using the values above
+        StockHolding input = new StockHolding(name, ticker, price);
 
         // Create your Execution Loop
-            // print the menu
-            // collect which option they chose from Scanner
-            // perform the operation: Check your balance, buy, sell, or exit
-            // repeat!
+        displayMenu();
+        int number = Integer.parseInt(in.nextLine());
 
-        System.out.println("Goodbye!");
+        while (!(number <= 4 && number > 0)) {
+            System.out.println("Selection is unrecognized. Please select a number from 1 to 4.");
+            displayMenu();
+            number = Integer.parseInt(in.nextLine());
+        }
+
+        while (true) {
+            if (number == 1) {
+                System.out.println("You own " + input.getNumShares() + " shares of " + input.getName() + " (" + input.getTicker() + ")");
+                System.out.println("Your balance is: " + "$" + input.getBalance());
+                displayMenu();
+                number = Integer.parseInt(in.nextLine());
+
+            } else if (number == 2) {
+                System.out.println("How many shares would you like to buy?");
+                int numberTwo = Integer.parseInt(in.nextLine());
+                input.buy(numberTwo);
+                displayMenu();
+                number = Integer.parseInt(in.nextLine());
+
+            } else if (number == 3) {
+                System.out.println("How many shares would you like to sell?");
+                int numberThree = Integer.parseInt(in.nextLine());
+                input.sell(numberThree);
+                displayMenu();
+                number = Integer.parseInt(in.nextLine());
+
+            } else if (number == 4) {
+                System.out.println("Goodbye!");
+                break;
+            }
+            while (!(number <= 4 && number > 0)) {
+                System.out.println("Selection is unrecognized. Please select a number from 1 to 4.");
+                displayMenu();
+                number = Integer.parseInt(in.nextLine());
+            }
+        }
     }
 }
--- src/main/java/com/kenzie/app/StockHolding.java
+++ src/main/java/com/kenzie/app/StockHolding.java
@@ -2,11 +2,62 @@ package com.kenzie.app;
 
 public class StockHolding {
     // Create private instance properties
+    private String name;
+    private String ticker;
+    private double price;
+    private int numShares;
 
     // Create constructor
+    public StockHolding (String name, String ticker, double price) {
+        this.name = name;
+        this.ticker = ticker;
+        this.numShares = 0;
+        this.price = price;
+    }
 
     // create getters
+    public String getName() {
+        return this.name;
+    }
+
+    public String getTicker() {
+        return this.ticker;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public int getNumShares() {
+        return this.numShares;
+    }
 
     // create buy, sell, and getBalance methods
+    public void buy(int numShares) {
+        if (numShares <= 0) {
+            this.numShares = 0;
+            System.out.println("You bought " + this.numShares + " shares for $" + this.numShares * getPrice());
+        } else {
+            this.numShares = this.numShares + numShares;
+            System.out.println("You bought " + numShares + " shares for $" + numShares * getPrice());
+        }
+    }
 
+    public double sell(int numShares) {
+        if (this.numShares > 0 && numShares <= this.numShares && numShares > 0) {
+            this.numShares = this.numShares - numShares;
+            System.out.println("You sold " + numShares + " shares for $" + numShares * getPrice());
+            return numShares * this.price;
+        } else if (numShares <= 0) {
+            System.out.println("Invalid number");
+            return 0;
+        } else {
+            System.out.println("You do not have enough shares!");
+            return 0;
+        }
+    }
+
+    public double getBalance() {
+        return numShares * this.price;
+    }
 }
